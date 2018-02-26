3404f766b5205896b53c61618bc10e2b72bf4132
Ahmad Ghallab <ahmadghallab@outlook.com>
2018-02-26 07:26:31 +0200

    Convert PUT /todos/:id to sequelize

--- server.js
+++ server.js
@@ -93,31 +93,28 @@ app.delete('/todos/:id', (req, res) => {
 // PUT /todos/:id
 app.put('/todos/:id', (req, res) => {
   let todoId = parseInt(req.params.id);
-  let matchedTodo = underscore.findWhere(todos, {id: todoId});
   let body = underscore.pick(req.body, 'description', 'completed');
-  let validAttributes = {};
-
-  if (!matchedTodo) {
-    return res.status(404).send();
+  let attributes = {};
+  if(body.hasOwnProperty('completed')) {
+    attributes.completed = body.completed;
   }
-
-  if(body.hasOwnProperty('completed') && underscore.isBoolean(body.completed)) {
-    validAttributes.completed = body.completed;
-  } else if(body.hasOwnProperty('completed')) {
-    return res.status(400).send();
+  if(body.hasOwnProperty('description')) {
+    attributes.description = body.description;
   }
-
-  if(body.hasOwnProperty('description') &&
-    underscore.isString(body.description) &&
-    body.description.trim().length > 0) {
-    validAttributes.description = body.description;
-  } else if(body.hasOwnProperty('description')) {
-    return res.status(400).send()
-  }
-
-  underscore.extend(matchedTodo, validAttributes);
-  res.json(matchedTodo)
-})
+  db.todo.findById(todoId).then((todo) => {
+    if(todo) {
+      todo.update(attributes).then((todo) => {
+        res.json(todo.toJSON());
+      }, (e) => {
+        res.status(400).json(e);
+      });
+    } else {
+      res.status(404).send();
+    }
+  }, () => {
+    res.status(500).send();
+  });
+});
 
 db.sequelize.sync().then(() => {
   app.listen(PORT, () => {
