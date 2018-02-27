220c405acf3cf9c73229a340f9803f00469a609b
Ahmad Ghallab <ahmadghallab@outlook.com>
2018-02-27 07:53:06 +0200

    Update last commit

--- server.js
+++ server.js
@@ -156,7 +156,7 @@ app.post('/users/login', (req, res) => {
 
 });
 
-db.sequelize.sync().then(() => {
+db.sequelize.sync({force: true}).then(() => {
   app.listen(PORT, () => {
     console.log('Express listening on port' + PORT + '!');
   });
